baa2240dd695af944b9bf08e3d1a95c45923e868
Anto <chue153204@gmail.com>
2026-08-31 18:29:48 +0630

    Create authConfig.js

---
+++ src/authConfig.js
@@ -0,0 +1,3 @@
+// Google Cloud Console > Credentials > OAuth client ID (Web application)
+// မှရတဲ့ Client ID ကို အောက်မှာထည့်ပါ။ မထည့်ရသေးရင် Guest နဲ့ဝင်သုံးနိုင်ပါတယ်။
+export const GOOGLE_WEB_CLIENT_ID = 'PASTE_YOUR_WEB_CLIENT_ID_HERE';
